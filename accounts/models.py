7995c6bcfb543f330d6ddd0f2fee4b07e73cee3f
Serj <jonatha-dias@outlook.com>
2024-11-14 19:16:29 -0300

    Nenhuma mudança concluida

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user_avatar = models.ImageField(default='default.jpg', upload_to='profile_images', verbose_name='Foto Perfil')
+    user_fname = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_fname', verbose_name='Primeiro Nome')
+    user_lname = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_lname', verbose_name='Último Nome')
+    
+    def __str__(self):
+        return self.userusername
+    
+
+
--- poderoso_apps/admin.py
+++ poderoso_apps/admin.py
@@ -1,7 +1,8 @@
 # Importa o módulo admin do Django, que fornece funcionalidades para criar interfaces administrativas.
 from django.contrib import admin  
 # Importa os modelos que serão registrados no painel administrativo.
-from .models import Topic, Entry, PlanoTreino, Exercicio  
+from .models import Topic, Entry, PlanoTreino, Exercicio
+from accounts.models import Profile
 
 # Registra o modelo Topic no painel administrativo do Django.
 admin.site.register(Topic)  
@@ -12,3 +13,4 @@ admin.site.register(PlanoTreino)
 # Registra o modelo Exercicio no painel administrativo do Django.
 admin.site.register(Exercicio)  
 # Comentário indicando que aqui é o local para registrar modelos adicionais, se necessário.
+admin.site.register(Profile)
